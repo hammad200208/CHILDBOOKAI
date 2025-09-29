9c9770e6b8117a7dad6c121e8f8f36585a4fc555
Hammad Khan <hamdev73@gmail.com>
2025-09-29 15:49:29 +0500

    add vercel.json

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 export const generateBook = async (prompt) => {
   try {
-    const response = await fetch("http://localhost:5000/api/book/generate", {
+    const response = await fetch("https://childbookai.vercel.app/api/book/generate", {
+    // const response = await fetch("http://localhost:5000/api/book/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
